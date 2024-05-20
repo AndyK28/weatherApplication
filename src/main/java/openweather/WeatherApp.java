2714220ee14344a901bb1b7c87f12be23228f1b8
akhumalo023a <akhumalo023a@student.wethinkcode.co.za>
2024-05-20 11:01:31 +0200

    basic structure for weather app

---
+++ src/main/java/openweather/WeatherApp.java
@@ -0,0 +1,37 @@
+package openweather;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class WeatherApp {
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        String apiKey = dotenv.get("API_KEY");
+
+        int cityId = 993800;
+
+        try {
+            URL url = new URL("http://api.openweathermap.org/data/2.5/weather?id=" + cityId + "&appid=" + apiKey);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            System.out.println(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
